28108dd540ebcf43997c25ff5adef49ebb00949d
jamtuba <jamtuba@gmail.com>
2020-10-23 13:54:04 +0200

    Added dragaction to the list of typeahead

--- BlazorMovies/Shared/Entities/Person.cs
+++ BlazorMovies/Shared/Entities/Person.cs
@@ -11,5 +11,19 @@ namespace BlazorMovies.Shared.Entities
         public string Picture { get; set; }
         [Required]
         public DateTime? DatoOfBirth { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is Person p2)
+            {
+                return Id == p2.Id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
